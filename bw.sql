fb4aee260bfdbac208720ee14a6db451c4414b0d
qinmengyang <meng1523562@qq.com>
2019-05-27 10:55:33 +0800

    bw.sql

---
+++ bw.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version phpStudy 2014
+-- http://www.phpmyadmin.net
+--
+-- 主机: localhost
+-- 生成日期: 2018 年 06 月 08 日 11:01
+-- 服务器版本: 5.5.47
+-- PHP 版本: 5.3.29
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- 数据库: `nw`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `data`
+--
+
+CREATE TABLE IF NOT EXISTS `data` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `head` varchar(255) DEFAULT NULL,
+  `tel` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `talk` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- 转存表中的数据 `data`
+--
+
+INSERT INTO `data` (`ID`, `head`, `tel`, `name`, `talk`, `username`) VALUES
+(2, NULL, NULL, NULL, NULL, '13466675502');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `message`
+--
+
+CREATE TABLE IF NOT EXISTS `message` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `message` varchar(255) DEFAULT NULL,
+  `date` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- 转存表中的数据 `message`
+--
+
+INSERT INTO `message` (`ID`, `title`, `message`, `date`, `image`) VALUES
+(2, NULL, 'afdaf', '1528347661', '../img/15283476610.jpg'),
+(3, NULL, '斯塔基要', '1528348051', '../img/15283480510.jpg');
+
+-- --------------------------------------------------------
